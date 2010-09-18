311dd79904e6cf996b41b7e15783d1e123afeee6
probertson <paul@probertson.com>
2010-09-18 16:28:19 -0500

    Make db contacts match xml contacts

--- src/sql/create/PopulateContacts.sql
+++ src/sql/create/PopulateContacts.sql
@@ -1,3 +1,5 @@
 ﻿INSERT INTO main.contacts (contactId, firstName, lastName, address, city, state, zip, phone, email)
-SELECT 1, 'Joel', 'Hooks', '2222 A St.', 'Fort Worth', 'TX', '76112', '817-555-1432', 'joelhooks@gmail.com' UNION
-SELECT 2, 'Paul', 'Robertson', '1401 B St.', 'Dublin', 'CA', '94568', '925-555-1110', 'paul@probertson.com'
+SELECT 1, 'Tariq', 'Ahmed', '1111 A St.', 'Anywhere', 'CA', '96112', '555-555-1111', 'tariq@example.com' UNION
+SELECT 2, 'John', 'Bland', '2222 B St.', 'Somewhere', 'NY', '12121', '555-555-1212', 'johnbland@example.com' UNION
+SELECT 3, 'Dan', 'Orlando', '3333 C St.', 'Nowhere', 'FL', '32568', '555-555-1313', 'dan.orlando@example.com' UNION
+SELECT 4, 'Joel', 'Hooks', '4444 A St.', 'Fort Worth', 'TX', '76112', '555-555-1414', 'joelhooks@gmail.com'
